aa36371c35cc7bf48f7f48b4ee95d9cd08737309
Wisely <wiselychung96@gmail.com>
2021-12-27 21:14:23 +0800

    finished proj, just expand.

--- js/pipboy.app.js
+++ js/pipboy.app.js
@@ -27,5 +27,31 @@ $(document).ready(function(){
         "weight": 13.1,
         "value": 144
     }
-    ]
+    ];
+
+    $('.item-list a').on('click', function(e){
+        $('.item-list a').removeClass('active');
+        $(e.currentTarget).addClass('active');
+    });
+
+    $('.item-list a').on('mouseenter', function(e){
+        var current_item = $(e.currentTarget).attr('class');
+        console.log(current_item);
+
+        for(item in weapons){
+            if(weapons[item].name == current_item){
+                console.log(weapons[item]);
+
+                var container = $('.item-stats');
+                container.find('.damage').html(weapons[item].damage);
+                container.find('.fire_rate').html(weapons[item].fire_rate);
+                container.find('.accuracy').html(weapons[item].accuracy);
+                container.find('.range').html(weapons[item].range);
+                container.find('.weight').html(weapons[item].weight);
+                container.find('.value').html(weapons[item].value);
+            }
+        }
+    });
+
+
 })
